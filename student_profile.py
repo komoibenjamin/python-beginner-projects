07dcbaa87547fe1e069201fe50bd41169bfc5c57
komoibenjamin <110690302+komoibenjamin@users.noreply.github.com>
2026-09-21 13:05:33 +0300

    Add student profile code

--- student_profile.py
+++ student_profile.py
@@ -1,1 +1,11 @@
+name = "Benjamin"
+age = 28
+course = "BSc Computer Technology"
+university = "Maseno University"
 
+print("Student Profile")
+print("----------------")
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("University:", university)
