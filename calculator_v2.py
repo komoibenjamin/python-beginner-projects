be56acbd83a913d709190f92cf98c0658c349f7b
komoibenjamin <110690302+komoibenjamin@users.noreply.github.com>
2026-09-21 16:08:08 +0300

    Add addition operation to calculator

--- calculator_v2.py
+++ calculator_v2.py
@@ -1,3 +1,6 @@
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 operation = input("Enter operation (+, -, *, /): ")
+if operation == "+":
+    result = first_number + second_number
+    print("Result:", result)
