416a37bf2480cd47f87be8af94d8c3274734de3d
komoibenjamin <110690302+komoibenjamin@users.noreply.github.com>
2026-09-21 15:59:19 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,13 @@
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
 
+addition = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+
+print("\n--- Calculator Results ---")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
